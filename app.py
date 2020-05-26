c518af9ccaaaef5bcaf97dfe3287f730026390de
AndrewJHinh <40700820+AndrewJHinh@users.noreply.github.com>
2020-05-25 20:13:58 -0700

    Update app.py

--- app.py
+++ app.py
@@ -13,13 +13,18 @@ import time
 import uuid
 import base64
 import tensorflow as tf 
+from keras import backend as K
+import keras
+import sys
+import matplotlib.pyplot as plt
+from PIL import Image
 
 img_width, img_height = 28, 28
 model_path = './model/model.hdf5'
 model =  tf.keras.models.load_model(model_path)
 
 UPLOAD_FOLDER = 'uploads'
-ALLOWED_EXTENSIONS = set(['png', 'jpeg', 'jpg', 'png'])
+ALLOWED_EXTENSIONS = set(['png', 'jpeg', 'jpg', 'pdf'])
 
 def get_as_base64(url):
     return base64.b64encode(request.get(url).content)
@@ -30,13 +35,6 @@ def predict(file):
     x = np.expand_dims(x, axis=0)
     array = model.predict(x)
     result = array[0]
-    import random as rnd
-    numList = result.tolist()
-    # calculate the normalizer, using: (1 / (sum_of_all_items_in_list))
-    normalizer = 1 / float( sum(numList) )
-    # multiply each item by the normalizer
-    numListNormalized = [x * normalizer for x in numList]
-    print(numListNormalized) #HOW TO DISPLAY THIS (SHOWS PROBS OF PREDS)
     answer = np.argmax(result)
     if answer == 0:
         print("Label: Bacterial Pneumonia")
@@ -46,6 +44,39 @@ def predict(file):
 	    print("Label: Normal")
     elif answer == 3:
 	    print("Label: Viral Pneumonia")
+    session = keras.backend.get_session()
+    init = tf.compat.v1.global_variables_initializer()
+    session.run(init)
+
+    class_output = model.output[:, answer]
+    last_conv_layer = model.get_layer('conv2d_15')
+    class_output = model.output[:, answer]
+    grads = K.gradients(class_output, last_conv_layer.output)[0]
+    pooled_grads = K.mean(grads, axis=(0, 1, 2))
+    iterate = K.function([model.input], [pooled_grads, last_conv_layer.output[0]])
+    pooled_grads_value, conv_layer_output_value = iterate([x])
+    for i in range(128):
+        conv_layer_output_value[:, :, i] *= pooled_grads_value[i]
+    heatmap = np.mean(conv_layer_output_value, axis=-1)
+    for x in range(heatmap.shape[0]):
+        for y in range(heatmap.shape[1]):
+            heatmap[x,y] = np.max(heatmap[x,y],0)
+
+    heatmap = np.maximum(heatmap,0)
+    heatmap /= np.max(heatmap)
+    plt.imshow(np.real(heatmap))
+
+    x = load_img(file, target_size=(28,28))
+    import skimage
+    upsample = skimage.transform.resize(heatmap, (28,28),preserve_range=True)
+    plt.imshow(x)
+    plt.imshow(upsample,alpha=0.5)
+    plt.tick_params(axis='both', which='both', bottom='off', top='off', labelbottom='off', right='off', left='off', labelleft='off')
+    plt.savefig('./uploads/foo.png', bbox_inches='tight', pad_inches = 0, transparent = True)
+    
+    image = Image.open('./uploads/foo.png')
+    image.thumbnail((1024, 1024))
+    image.save('./uploads/foo.png')
     return answer
 
 def my_random_string(string_length=10):
@@ -64,7 +95,7 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/")
 def template_test():
-    return render_template('template.html', label='', imagesource='../uploads/download.jpg')
+    return render_template('template.html', label='', imagesource='../uploads/person1_bacteria_1.jpeg')
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
@@ -93,7 +124,8 @@ def upload_file():
 
             os.rename(file_path, os.path.join(app.config['UPLOAD_FOLDER'], filename))
             print("--- %s seconds ---" % str (time.time() - start_time))
-            return render_template('template.html', label=label, imagesource='../uploads/' + filename)
+
+            return render_template('template.html', label=label, imagesource='./uploads/foo.png')#'../uploads/' + filename)
 
 from flask import send_from_directory
 
